6d27b3c3e83d816f9882fb686844ba47888b3d49
benjamingarciap <bengarc@gmail.com>
2021-01-01 19:48:30 +0100

    Updates

---
+++ precache-manifest.61281253473a2ff9b0f61eb40d6b500a.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e152df2d7e01e6eaf71bf58029a7815f",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "9fb18b2ef62ab5ab064a",
+    "url": "/personal-portfolio/static/css/main.8507f443.chunk.css"
+  },
+  {
+    "revision": "f9e7876a98df42e1a578",
+    "url": "/personal-portfolio/static/js/2.6b3a98a8.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/personal-portfolio/static/js/2.6b3a98a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb18b2ef62ab5ab064a",
+    "url": "/personal-portfolio/static/js/main.b036f10d.chunk.js"
+  },
+  {
+    "revision": "ea168963229c4dffc4d2",
+    "url": "/personal-portfolio/static/js/runtime-main.6d82f845.js"
+  },
+  {
+    "revision": "e1faeee751b18cae72e50a69fd2bb0b8",
+    "url": "/personal-portfolio/static/media/1.e1faeee7.jpg"
+  },
+  {
+    "revision": "6dba580bc02321b21c40c98ebf4bc6b9",
+    "url": "/personal-portfolio/static/media/2.6dba580b.jpg"
+  },
+  {
+    "revision": "8cdaf32efffefc373ee9c5ae5c65a3f6",
+    "url": "/personal-portfolio/static/media/3.8cdaf32e.jpg"
+  },
+  {
+    "revision": "b61e5ebed9dc7b2c0321cf8fa9098234",
+    "url": "/personal-portfolio/static/media/4.b61e5ebe.jpg"
+  },
+  {
+    "revision": "4abec7fd006d1d0627889ef3e16d4892",
+    "url": "/personal-portfolio/static/media/5.4abec7fd.jpg"
+  },
+  {
+    "revision": "5361433a1fd4c20ee57102ac31f4f77c",
+    "url": "/personal-portfolio/static/media/CV.5361433a.pdf"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/personal-portfolio/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "c0dd94eb6aa23f646c260731accb6ca8",
+    "url": "/personal-portfolio/static/media/adobe.c0dd94eb.svg"
+  },
+  {
+    "revision": "03b9340c75f75149e1a8dd3edcd251df",
+    "url": "/personal-portfolio/static/media/codepen.03b9340c.svg"
+  },
+  {
+    "revision": "3fcc1d5aa04e51f43453caeab9ea11f1",
+    "url": "/personal-portfolio/static/media/css.3fcc1d5a.svg"
+  },
+  {
+    "revision": "cc292f1bc2bc8745986fee11807110fe",
+    "url": "/personal-portfolio/static/media/devdesign.cc292f1b.svg"
+  },
+  {
+    "revision": "e6d28ff2b63a7877b41a7ef3da0f6a7c",
+    "url": "/personal-portfolio/static/media/express.e6d28ff2.svg"
+  },
+  {
+    "revision": "ae8f6dfdf7a68d39dd24bbbe94c0468d",
+    "url": "/personal-portfolio/static/media/figma.ae8f6dfd.svg"
+  },
+  {
+    "revision": "bd265277c53b322b86059c62c6021b46",
+    "url": "/personal-portfolio/static/media/git.bd265277.svg"
+  },
+  {
+    "revision": "9d1a54f0968f30741204bee785c4df12",
+    "url": "/personal-portfolio/static/media/github.9d1a54f0.svg"
+  },
+  {
+    "revision": "123d646ab8188ef05d79eaad11c20076",
+    "url": "/personal-portfolio/static/media/html.123d646a.svg"
+  },
+  {
+    "revision": "2438237de2e9fad67a868bf5cfd721c6",
+    "url": "/personal-portfolio/static/media/javascript.2438237d.svg"
+  },
+  {
+    "revision": "f88add1f5514ab4efebdd1fb5d92ae03",
+    "url": "/personal-portfolio/static/media/linkedin.f88add1f.svg"
+  },
+  {
+    "revision": "97e8f8bb3167dd7a7ce430d19307e81a",
+    "url": "/personal-portfolio/static/media/logo.97e8f8bb.svg"
+  },
+  {
+    "revision": "9bfbf083aaf44e9e63ea0f8e78a675e4",
+    "url": "/personal-portfolio/static/media/mongodb.9bfbf083.svg"
+  },
+  {
+    "revision": "6d735cf935861d453f372523fa3ce8a5",
+    "url": "/personal-portfolio/static/media/node.6d735cf9.svg"
+  },
+  {
+    "revision": "5aec709c983e6e990d503f99bbff0fff",
+    "url": "/personal-portfolio/static/media/react.5aec709c.svg"
+  },
+  {
+    "revision": "5248922a6fd9e4b3ca74f49c36d4dc0f",
+    "url": "/personal-portfolio/static/media/redux.5248922a.svg"
+  },
+  {
+    "revision": "705798e40c95185bdbed1cf48b1ee468",
+    "url": "/personal-portfolio/static/media/rooms-1.705798e4.jpg"
+  },
+  {
+    "revision": "cee9d3c417d8fba7a498882fc972872f",
+    "url": "/personal-portfolio/static/media/rooms-2.cee9d3c4.jpg"
+  },
+  {
+    "revision": "7bb65a463aa76643929722bef98217f1",
+    "url": "/personal-portfolio/static/media/rooms-3.7bb65a46.jpg"
+  },
+  {
+    "revision": "ff0aa641c0223bd4d9fa653535b3ce8d",
+    "url": "/personal-portfolio/static/media/rooms-4.ff0aa641.jpg"
+  },
+  {
+    "revision": "8c688dbf085e45b7c1cd279e55919a64",
+    "url": "/personal-portfolio/static/media/rooms-5.8c688dbf.jpg"
+  },
+  {
+    "revision": "476efed200768ec6b1d015090d3f54c3",
+    "url": "/personal-portfolio/static/media/softskills.476efed2.svg"
+  },
+  {
+    "revision": "c5beb8f6623200a4839141551c27895d",
+    "url": "/personal-portfolio/static/media/typescript.c5beb8f6.svg"
+  }
+]);
