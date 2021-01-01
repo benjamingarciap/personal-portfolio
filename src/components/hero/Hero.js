f97a5e92404597f46bd962115f7a83fdbc3ebec9
benjamingarciap <bengarc@gmail.com>
2021-01-01 20:07:57 +0100

    fix hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -14,23 +14,23 @@ class Hero extends Component {
     this.vantaEffect = NET({
       el: this.vantaRef.current,
       THREE: THREE,
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.00,
-      minWidth: 200.00,
-      scale: 1.00,
-      scaleMobile: 1.00
       // mouseControls: true,
       // touchControls: true,
       // gyroControls: false,
       // minHeight: 200.00,
       // minWidth: 200.00,
       // scale: 1.00,
-      // scaleMobile: 1.00,
-      // points: 5.00,
-      // maxDistance: 24.00,
-      // spacing: 13.00
+      // scaleMobile: 1.00
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      points: 5.00,
+      maxDistance: 24.00,
+      spacing: 13.00
     })
   }
   componentWillUnmount() {
